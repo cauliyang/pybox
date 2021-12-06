5aa378d29f87688f356e1999d1554af1e9309dcb
yangli <li002252@umn.edu>
2021-12-05 20:05:11 -0600

    fix a bug about nox docs

--- noxfile.py
+++ noxfile.py
@@ -182,7 +182,7 @@ def docs_build(session: Session) -> None:
         args.insert(0, "--color")
 
     session.install(".")
-    session.install("sphinx", "sphinx-click", "furo")
+    session.install("sphinx", "sphinx-click", "furo", "myst_parser")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
@@ -196,7 +196,7 @@ def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
     session.install(".")
-    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "furo")
+    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "furo", "myst_parser")
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
