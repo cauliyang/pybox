5002e6a3cad972d3e14d9428d09df89bbadfad52
yangli <li002252@umn.edu>
2022-09-14 22:03:17 -0500

    [git actions] add pyyaml

--- noxfile.py
+++ noxfile.py
@@ -200,7 +200,9 @@ def docs(session: Session) -> None:
     """Build and serve the documentation with live reloading on file changes."""
     args = session.posargs or ["--open-browser", "docs", "docs/_build"]
     session.install(".")
-    session.install("sphinx", "sphinx-autobuild", "sphinx-click", "furo", "myst_parser")
+    session.install(
+        "sphinx", "sphinx-autobuild", "sphinx-click", "furo", "myst_parser", "pyyaml"
+    )
 
     build_dir = Path("docs", "_build")
     if build_dir.exists():
